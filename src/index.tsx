49492cac6ea98465af3f8ff89a45d621ebb27db1
Isaac Mahgrefteh and Michael Van Meurs <pair+isaac.mahgrefteh+michael-vanmeurs@pivotal.io>
2018-07-17 11:50:43 -0700

    add css

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import "./styles/main.scss";
 
 import {Allocations} from "./components/Allocations";
 import {Challenge, CompanyRole, TeamRole} from "./definitions";
--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,33 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 module.exports = {
-    entry: "./src/index.tsx",
-    output: {
-        filename: "bundle.js",
-        path: __dirname + "/dist"
-    },
+  entry: "./src/index.tsx",
+  output: {
+    filename: "[name].js",
+    path: __dirname + "/dist"
+  },
+  devtool: "source-map",
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".json", ".css", ".scss"]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({filename: '[name].css'})
+  ],
+  module: {
+    rules: [
+      {test: /\.tsx?$/, loader: "awesome-typescript-loader"},
+      {test: /\.css$/, use: [{loader: MiniCssExtractPlugin.loader, options: {publicPath: '../'}}, 'css-loader']},
+      {test: /\.(sass|scss)$/, use: [{loader: MiniCssExtractPlugin.loader, options: {publicPath: '../'}}, 'css-loader', 'sass-loader']},
+      {enforce: "pre", test: /\.js$/, loader: "source-map-loader"}
+    ]
+  },
 
-    // Enable sourcemaps for debugging webpack's output.
-    devtool: "source-map",
-
-    resolve: {
-        // Add '.ts' and '.tsx' as resolvable extensions.
-        extensions: [".ts", ".tsx", ".js", ".json"]
-    },
-
-    module: {
-        rules: [
-            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-            { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
-
-            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
-        ]
-    },
-
-    // When importing a module whose path matches one of the following, just
-    // assume a corresponding global variable exists and use that instead.
-    // This is important because it allows us to avoid bundling all of our
-    // dependencies, which allows browsers to cache those libraries between builds.
-    externals: {
-        "react": "React",
-        "react-dom": "ReactDOM"
-    }
+  // When importing a module whose path matches one of the following, just
+  // assume a corresponding global variable exists and use that instead.
+  // This is important because it allows us to avoid bundling all of our
+  // dependencies, which allows browsers to cache those libraries between builds.
+  externals: {
+    "react": "React",
+    "react-dom": "ReactDOM"
+  }
 };
